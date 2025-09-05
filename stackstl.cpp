e30e26fa7a0650aebfca4a2a5163bf600128b602
lalit-debug <lalitmalidz007@gmail.com>
2025-09-05 21:54:23 +0530

    adding stack ,queue stl file

---
+++ stackstl.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<string> s;
+    s.push("love");
+    s.push("babbar");
+    s.push("kumar");
+
+    cout<<"top element "<<s.top()<<endl;
+
+     s.pop();
+     cout<<"top element --> "<<s.top()<<endl;
+    
+     cout<<"size of stack"<<s.stack()<<endl;
+
+     cout<<"empty or not "<<s.empty()<<endl;
+     
+}
