d5bb94d661e5c1887e695c1f9e653c38da6dcc10
lalit-debug <lalitmalidz007@gmail.com>
2025-09-10 01:56:28 +0530

    Updated mergequestion.cpp with correct merge logic

--- lec 20/mergequestion.cpp
+++ lec 20/mergequestion.cpp
@@ -1,22 +1,24 @@
-        class Solution {
+class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i =0, j=0, k=0;
+        int i =m-1, j=n-1, k=m+n-1;
         
-        while(i<n && j<m){
-            if(nums1[i]=0)
-            if(nums1[i]<nums2[j]){
-                nums1[i++]=nums2[j++];
+        while(i>=0 && j>=0){
+        
+            if(nums1[i] > nums2[j]){
+                nums1[k--]=nums1[i--];
+                // nums1[k]=nums1[i];
+                // k++;
+                // i++;
+
+
             }
-            else(nums1[i]>nums2[j]){
-                nums1[i++]=nums2[j++];
+            else{
+                nums1[k--]=nums2[j--];
             }
         }
-        while(i<n){
-            nums1[i++]=nums2[j++];
-        }
-        while(j<m){
-            nums1[i++]=nums2[j++];
+          while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
     }
     void print(int ans[],int n){
@@ -25,8 +27,14 @@ public:
         }
     }
     int main(){
-        merge(nums1[i],n,nums[2],m,nums1[i]);
-        print(nums1[i],m+n);
+        vector<int> nums1;
+        int m ;
+        vector<int> nums2;
+         int n ;
+
+        merge(nums1,m,nums2,n);
+        print(nums1.data(), m+n);
+
         return 0;
     }
 };
